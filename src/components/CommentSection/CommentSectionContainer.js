c4a1975a95882436c7b0226e945e71122ca1674f
Carlos Abreu <c.abreu00@gmail.com>
2020-03-18 20:34:11 -0400

    Fixed to show comments

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -1,17 +1,20 @@
 // You will add code to this file
-import React from "react";
+import React, { useState } from "react";
 import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
+const [comments ] = useState(props.comments);
 
   return (
-    <div key ={props.postId}>
-      {props.comments.map((comment, index) => (
-        <comment key={index} comment ={comment} />
-      ))}
+    <div>
+      {
+        comments.map(comment => {
+        return <Comment comment={comment} />;
+          })
+      }
       {/* map through the comments data and return the Comment component */}
       <CommentInput />
     </div>
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,18 +1,22 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
 import dummyData from '../../dummy-data';
-const PostsPage = (props) => {
+
+
+const PostsPage = () => {
   // set up state for your data
+  const [data ] = useState(dummyData)
+  console.log(data)
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
-      {dummyData.map((post, index) => (
-        <Post key={index} post={post} />
-      ))}
+    {data.map(post => {
+      return <Post post={post} key={post.id} />;
+    })}
     </div>
   );
 };
